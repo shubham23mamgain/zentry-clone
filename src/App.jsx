5ed5c24f4899c2067ffb75caa96572e8f62c7488
Shubham Mamgain <41307085+shubham23mamgain@users.noreply.github.com>
2024-12-22 11:52:21 +0530

    initialzed repo

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import Story from "./components/Story";
+
+const App = () => {
+  return (
+    <main className="realtive min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
